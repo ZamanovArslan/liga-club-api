6273c4a0e42eb12658c88980f05eada6923c0a8a
ArslanZamanov <arslan.zamanov@flatstack.com>
2020-08-20 00:10:24 +0300

    Add name length validation to badge and level

--- app/models/badge.rb
+++ app/models/badge.rb
@@ -12,6 +12,9 @@ class Badge < ApplicationRecord
   scope :ended, -> { where("ends_at < ?", Time.zone.now) }
   scope :actual, -> { where("ends_at > ?", Time.zone.now) }
 
+  validates :name, presence: true
+  validates :name, length: { maximum: 30 }
+
   enumerize :confirmation_method, in: CONFIRMATION_METHODS, predicates: true, scope: true
 
   mount_uploader :image, BaseUploader
--- app/models/level.rb
+++ app/models/level.rb
@@ -1,5 +1,6 @@
 class Level < ApplicationRecord
   validates :name, :scores_count, presence: true
+  validates :name, length: { maximum: 20 }
 
   mount_uploader :image, BaseUploader
 end
