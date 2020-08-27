8ba23481748384dfafc78c8b475c54a733d3e42e
ArslanZamanov <arslan.zamanov@flatstack.com>
2020-08-27 20:10:40 +0300

    Add university student validation to participation

--- app/models/participation.rb
+++ app/models/participation.rb
@@ -5,6 +5,15 @@ class Participation < ApplicationRecord
   scope :confirmed, -> { where(confirmed: true) }
 
   validates :user, uniqueness: { scope: :badge }
+  validate :user_should_be_from_badge_university, if: -> { badge.university }
 
   mount_uploader :attachment_confirmation, BaseUploader
+
+  private
+
+  def user_should_be_from_badge_university
+    unless badge.university == user.university
+      errors.add(:badge, :not_a_student, { university_name: badge.university.name })
+    end
+  end
 end
